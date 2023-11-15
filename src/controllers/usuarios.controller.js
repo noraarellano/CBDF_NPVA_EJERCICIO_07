2ed1a8f74dd260abba1721a458cfb66004ac1b2b
noraarellano <72581638+noraarellano@users.noreply.github.com>
2023-11-14 22:02:38 -0600

    Update usuarios.controller.js

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -88,7 +88,7 @@ exports.updateUser = async(req, res)=>{
             })
         }else{
             //Se requiere escribir la nueva clave 
-            const salt = await bcryp.hash.genSalt(8);
+            const salt = await bcrypt.hash.genSalt(8);
             const claveEncriptada =await bcrypt.hash(clave, salt);
             await Usuarios.findOneAndUpdate({correo:correo},{nombres:nombres, apellidos:apellidos, clave:claveEncriptada})
             res.status(200).json({
